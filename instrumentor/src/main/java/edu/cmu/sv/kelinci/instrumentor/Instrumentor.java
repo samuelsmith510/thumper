dcd0aefa375e9e5c8a0cf80ac03d7eb228ba8730
Joshua Pereyda <jtpereyda@users.noreply.github.com>
2019-04-26 16:58:33 -0700

    "Hint: "

--- instrumentor/src/main/java/edu/cmu/sv/kelinci/instrumentor/Instrumentor.java
+++ instrumentor/src/main/java/edu/cmu/sv/kelinci/instrumentor/Instrumentor.java
@@ -131,7 +131,7 @@ public class Instrumentor {
 	private static void loadAndWriteResource(String resource) {
 		InputStream is = classloader.getResourceAsStream(resource);
 		if (is == null) {
-			System.err.println(String.format("Error loading Kelinci class for addition to output (did you modify the Kelinci class and not modify the Instrumentor.java resources list?): %s", resource));
+			System.err.println(String.format("Error loading Kelinci class for addition to output (Hint: Did you modify the Kelinci class and not modify the Instrumentor.java resources list?): %s", resource));
 			return;
 		}
 		byte[] rbytes;
